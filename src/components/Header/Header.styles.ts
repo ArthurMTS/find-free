55ed2171d2d09df0adb5daf5efbb16ee01b9e14e
Arthur Silva <arthurmatsilva@outlook.com>
2021-12-23 14:31:01 -0300

    header component and css reset

---
+++ src/components/Header/Header.styles.ts
@@ -0,0 +1,18 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export const useStyles = makeStyles({
+  header: {
+    backgroundColor: '#222',
+    padding: '10px 50px',
+    display: 'flex',
+    alignItems: 'center',
+    gap: '10px',
+  },
+  icon: {
+    fontSize: '5ch',
+    color: '#fff',
+  },
+  title: {
+    color: '#fff',
+  },
+});
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,15 @@
+import { Box } from '@material-ui/core';
+import { SportsEsports } from '@material-ui/icons';
+
+import { useStyles } from './Header.styles';
+
+export const Header = () => {
+  const styles = useStyles();
+
+  return (
+    <Box component='header' className={styles.header}>
+      <SportsEsports className={styles.icon} />
+      <h1 className={styles.title}>Find Free</h1>
+    </Box>
+  );
+}
